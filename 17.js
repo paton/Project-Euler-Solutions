af3b13ea9b61712e6d58ce909c6a06917249817b
Brandon Paton <bp@brandonpaton.com>
2013-07-30 16:52:56 -0700

    Finished 17

--- 17.js
+++ 17.js
@@ -7,6 +7,10 @@ If all the numbers from 1 to 1000 (one thousand) inclusive were written out in w
 NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
 */
 
+
+/// TODO: Refactor this. It's a messy solution
+
+
 var sumOfWordStrings = function(num) {
   var sumOfWords = 0,
       numWords, iString, numString = '';
@@ -22,6 +26,16 @@ var sumOfWordStrings = function(num) {
     7: 'seven',
     8: 'eight',
     9: 'nine',
+    00: '',
+    01: 'one',
+    02: 'two',
+    03: 'three',
+    04: 'four',
+    05: 'five',
+    06: 'six',
+    07: 'seven',
+    08: 'eight',
+    09: 'nine',
     10: 'ten',
     11: 'eleven',
     12: 'twelve',
@@ -41,7 +55,7 @@ var sumOfWordStrings = function(num) {
     80: 'eighty',
     90: 'ninety',
     100: 'hundred',
-    1000: 'thousand'
+    1000: 'onethousand'
   }
 
   for (var i = 1; i <= num; i++) {
@@ -54,14 +68,19 @@ var sumOfWordStrings = function(num) {
       if (i > 99) numString += numWords[iString[iString.length - 3]] + numWords[100];
 
       if (i > 19) {
-        if (i > 99) numString += "and";
-        numString += numWords[iString[iString.length - 2] + '0'] + numWords[iString[iString.length - 1]];
+        if (i > 99 && i % 100 !== 0) numString += "and";
+        if (numWords[iString.slice(iString.length-2, iString.length)])
+          numString += numWords[iString.slice(iString.length-2, iString.length)];
+        else
+          numString += numWords[iString[iString.length - 2] + '0'] + numWords[iString[iString.length - 1]];
       } else if (i > 9) {
         numString += "and" + numWords[iString.slice(iString.length - 2, iString.length)];
       } else if (i > 0) {
         numString += "and" + numWords[iString.slice(iString.length - 1, iString.length)]
       }
-      console.log(numString)
+      numString = numString.replace('undefined', '');
+      console.log()
+      sumOfWords += numString.length;
       numString = ''
     }
 
@@ -69,8 +88,6 @@ var sumOfWordStrings = function(num) {
 
   return sumOfWords;
 }
-
-9996
 
 console.log(sumOfWordStrings(1000))
 
